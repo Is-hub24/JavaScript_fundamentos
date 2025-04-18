d92d545b1104418b511767955f771527503bb93c
Isis Lugtenburg <isislugtenburg322@gmail.com>
2025-04-18 15:17:34 -0300

    Aula 1

---
+++ type_string.js
@@ -0,0 +1,17 @@
+// type String
+
+const texto1 = "Olá Mundo!";
+const texto2 = "Olá Mundo!";
+const senha = "admin123";
+const stringNumeros = "3415";
+
+const citacao = "Nome nome é:"
+const meuNome = "Leonardo"
+console.log(citacao + meuNome)
+
+// concatenação (+)
+
+
+
+
+// template string ou template literal
