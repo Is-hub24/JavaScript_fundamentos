04d0abbdbed37722c9e4f54be75f7a755edc6ef0
Isis Lugtenburg <isislugtenburg322@gmail.com>
2025-04-21 01:19:59 -0300

    Add de novos projetos para estudar outras regras basicas no JS

---
+++ conversoes.js
@@ -0,0 +1,22 @@
+// tipo de dado
+// boolean
+
+// conversão implicita
+const numero = 456;
+const numeroString = "456"
+
+// = recebe o valor
+// == compara o valor
+// === 
+console.log(numero == numeroString)
+
+// string + string
+console.log(numero + numeroString)
+
+
+
+// conversoes explicita
+
+// number() -> também funciona dentro da variavel ex: const numeroString = Number("456")
+// string()
+console.log(numero + Number(numeroString))
